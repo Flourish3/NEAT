a9acef094eb2591047db2427b507f827521c49ec
Johan Tillmar <johan.tillmar@gmail.com>
2017-08-21 20:32:59 +0200

    Added mutation capbility through adding connection and node.

--- genome/genome.py
+++ genome/genome.py
@@ -58,11 +58,28 @@ class Genome:
                 if self.connections(i).output_node == connection.input_node:
                     self.process_connection(self.connections(i), i)        
 
-    def mate(self, other):
+    def crossover(self, other):
         pass
     
-    def add_node(self):
-        pass
-    
-    def add_connection(self):
+    #Adds a new node, splitting an old, random connection
+    def mutate_add_node(self,innov):
+        mutate_connection = self.connections[random.randrange(len(self.connections))]
+        mutate_connection.enabled = False
+
+        self.nodes[self.nbr_nodes] = node_gene(self.nbr_nodes,"Hidden") 
+        
+        self.connections.apped(connect_gene(mutate_connection.input_node, self.nodes[self.nbr_nodes],1,innov))
+
+        self.connections.append(connect_gene(self.nodes[self.nbr_nodes],mutate_connection.output_node, mutate_connection.weight,innov+1))
+
+        self.nbr_nodes += 1
+
+
+    def mutate_add_connection(self,innov):
+        node1 = self.nodes[random.randrange(self.nbr_nodes)]
+        node2 = self.nodes[random.randrange(self.nbr_nodes)]
+        weight = random.random()*2-1
+        self.connections.append(connect_gene(node1,node2,weight,innov))
+
+    def mutate_change_weights(self):
         pass
